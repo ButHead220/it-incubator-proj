e8abc1797265f8e8f860e8c4be3f5967d2c9d336
Abramov Egor <egor.abramov.19966@gmail.com>
2023-08-13 11:24:53 +0600

    homework it-incubator 2 week. change input-validation-middleware.ts for solve error with "errorsMessages" variable

--- src/middlewares/input-validation-middleware.ts
+++ src/middlewares/input-validation-middleware.ts
@@ -2,10 +2,10 @@ import {Request, Response, NextFunction} from "express";
 import {validationResult} from "express-validator";
 
 
-const errorsArray = ({message , field}: any) => {
+const errorFormatter = ({msg , path}: any) => {
     return {
-        message: message,
-        field: field,
+        message: msg,
+        field: path,
     }
 }
 
@@ -13,7 +13,7 @@ export const inputValidationMiddleware = (req: Request, res: Response, next: Nex
     const errors = validationResult(req)
 
     if(!errors.isEmpty()) {
-        const errorsMessages = errors.array({onlyFirstError: true}).map(err => errorsArray(err))
+        const errorsMessages = errors.array({onlyFirstError: true}).map(e => errorFormatter(e))
         res.status(400).json({errorsMessages})
     } else {
         next()
