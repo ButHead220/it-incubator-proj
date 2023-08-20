f428d0e4560f8d0b4dfba5e6908a71ec9a9e52ba
Abramov Egor <egor.abramov.19966@gmail.com>
2023-08-20 17:34:18 +0600

    homework it-incubator 3 week. refactor

--- src/middlewares/post-validation.ts
+++ src/middlewares/post-validation.ts
@@ -1,16 +1,17 @@
 import {body} from "express-validator";
 import {inputValidationMiddleware} from "./input-validation-middleware";
-import {postsCollection} from "../mongoDb";
+import {blogsCollection} from "../mongoDb";
 
 export const postsValidation = [
     body('title').isString().trim().isLength({min: 1, max: 30}).withMessage('incorrect title'),
     body('shortDescription').isString().trim().isLength({min: 1, max: 100}).withMessage('incorrect shortDescription'),
     body('content').isString().trim().isLength({min: 1, max: 1000}).withMessage('incorrect content'),
-    body('blogId').isString().trim().notEmpty().withMessage('incorrect blogId').custom((id) => {
-            const foundBlogId =  postsCollection.findOne({id: id})
+    body('blogId').isString().trim().notEmpty().withMessage('incorrect blogId').custom(async (id) => {
+            const foundBlogId =  await blogsCollection.findOne({id: id})
             if (!foundBlogId) {
                 throw new Error('blogId not found')
             } else {
+
                 return true
             }
         }
