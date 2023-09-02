7e9200dbf0638bb737bcd1492911647b58154151
Abramov Egor <egor.abramov.19966@gmail.com>
2023-09-02 21:51:24 +0600

    homework it-incubator 5 week. debugging

--- src/middlewares/user-validation.ts
+++ src/middlewares/user-validation.ts
@@ -2,7 +2,7 @@ import {body} from "express-validator";
 import {inputValidationMiddleware} from "./input-validation-middleware";
 
 export const userValidation = [
-    body('login').isString().trim().isLength({min: 3, max: 10}).matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/).withMessage('incorrect login'),
+    body('login').isString().trim().isLength({min: 3, max: 10}).matches(/^[a-zA-Z0-9_-]*$/).withMessage('incorrect login'),
     body('password').isString().trim().isLength({min: 6, max: 20}).withMessage('incorrect password'),
     body('email').isString().matches(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/).withMessage('incorrect email'),
     inputValidationMiddleware
